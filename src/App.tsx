910b72a909c64dc79913c594bb112b2f108932dc
joonmoogo <junemuk10@gmail.com>
2024-05-21 17:34:07 +0900

    Using Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Zustand from './Components/Zustand';
 import { MobxPage } from './Components/Mobx';
 import { RecoilPage } from './Components/Recoil';
-import Redux from './Components/Redux';
+import { ReduxPage } from './Components/Redux';
 import { ContextPage } from './Components/Context';
+import ZustandPage from './Components/Zustand';
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='zustand' element={<Zustand />} />
+        <Route path='zustand' element={<ZustandPage />} />
         <Route path='mobx' element={<MobxPage />} />
         <Route path='recoil' element={<RecoilPage />} />
-        <Route path='redux' element={<Redux />} />
+        <Route path='redux' element={<ReduxPage />} />
         <Route path='context' element={<ContextPage />} />
       </Routes>
     </>
--- src/Components/Redux.tsx
+++ src/Components/Redux.tsx
@@ -1,5 +1,29 @@
-export default function Redux(){
-    return(
-        <div>redux</div>
+import React from "react";
+import { Provider } from "react-redux";
+import { increment } from "../Util/ReduxCounterSlice";
+import { useAppSelector, useAppDispatch } from '../Util/ReduxHook'
+import { store } from "../Util/ReduxStore";
+
+export default function Redux() {
+
+    const count: number = useAppSelector(state => state.counter.value)
+    const dispatch = useAppDispatch();
+
+    const onClick = () => {
+        dispatch(increment());
+    }
+    return (
+        <React.Fragment>
+            <button onClick={onClick}>button</button>
+            <div>{count}</div>
+        </React.Fragment>
+    )
+}
+
+export function ReduxPage() {
+    return (
+        <Provider store={store}>
+            <Redux />
+        </Provider>
     )
 }
